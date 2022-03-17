147aa26b8ba7a7b890d6a16c7f7149affe31afeb
user42 <ssar@student.42.fr>
2022-03-17 10:21:14 +0100

    commentaires

---
+++ essais/manageServer/Server.cpp
@@ -0,0 +1,204 @@
+#include "Server.hpp"
+#include <iostream>
+
+Server::Server()
+{
+	this->listenfd = -1;
+}
+Server::Server(serverBlock block)
+{
+	this->listenfd = -1;
+	this->infoConfig = block;
+}
+
+Server::~Server()
+{
+	if (this->listenfd != -1)
+			close(this->listenfd);
+}
+
+void Server::clear_fd()
+{
+	if (this->listenfd != -1)
+			close(this->listenfd);
+}
+
+
+void Server::connect()
+{
+	struct sockaddr_in srv;
+	int reuse_addr = 1;
+
+	if ((this->listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+			exit(1);
+	setsockopt(this->listenfd, SOL_SOCKET, SO_REUSEADDR, &reuse_addr, sizeof(reuse_addr));
+	memset(srv.sin_zero, '\0', sizeof(srv.sin_zero));
+	srv.sin_family = AF_INET;
+	srv.sin_addr.s_addr = htonl(this->infoConfig.getHost());
+	srv.sin_port = htons(this->infoConfig.getPort());
+	if (bind(this->listenfd, (struct sockaddr *) &srv, sizeof(srv)) < 0)
+	{
+		std::cout << "Bind address already in use \n";
+		clear_fd();
+		exit(1);
+	}
+	if (listen(this->listenfd, 1024) < 0)
+	{
+		clear_fd();
+		exit(1);
+	}
+	
+}
+
+std::string Server::processContentLength(std::string request)
+{
+	std::string tmp = request.substr(request.find("Content-Length: ") + 15, request.length());
+	int index = request.find("\r\n\r\n");
+	std::string tmp1 = tmp.substr(0, index - 1);
+	std::string body = request.substr(index + 4, request.length() - (index + 4));
+
+	if (body.length() != atoi(tmp1.c_str()))
+		std::cout << "ERROR: Body length not as expected" << atoi(tmp1.c_str()) << std::endl; 
+	return(request + "\r\n\r\n");
+}
+
+
+std::string Server::processContent(std::string request, int fd, int epfd)
+{
+	int size = 24;
+	std::string bufStr;
+	char buf[size];
+	int check = 0;
+	check = recv(fd, buf, size - 1, 0);
+	while (check > 0)
+	{
+		request += buf;
+		memset(buf, '\0', size);
+		check = recv(fd, buf, size - 1, 0);
+	}
+	request += buf;
+	if (check == 0)
+	{
+		std::cout << "connexion close by client" << std::endl;
+		close (fd);
+		epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL);
+	}
+	if (request.find("Content-Length: ") == std::string::npos && (request.find_last_of("\r\n\r\n") != request.length() - 1))
+	{
+		int i = 0;
+		while (i < request.length() - 1)
+		{
+			std::cout << i << "is: " << request[i] << std::endl;
+			i++;
+		}
+		std::cout << "find last of is" << request.find_last_of("\r\n\r\n") << "LEN IS" << request.length() - 1 << std::endl;
+		std::cout << "error with recv" << std::endl;
+		close (fd);
+		epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL);
+	}
+	if (request.find("Transfer-Encoding: chunked") != std::string::npos)
+		return (chunkDecoder(request));
+	if (request.find("Content-Length: ") != std::string::npos)
+		return(processContentLength(request));
+	return (request);
+}
+
+std::string Server::chunkDecoder(std::string str)
+{
+	std::cout << "CHUNK DECODER" << std::endl;
+    //FROM BEGINNING OF REQUEST UNTIL THE END OF FIRST EMPTY LINE
+	std::string	head = str.substr(0,str.find("\r\n\r\n") + 4);
+    //FROM AFTER NEW LINE TO END OF MESSAGE
+	std::string	coded = str.substr(str.find("\r\n\r\n") + 4, str.length() - 1);
+	
+    std::string	subchunk = coded;
+	std::string	body;
+	// GET CHUNK SIZE FROM BASE 16 
+    int			chunksize = strtol(coded.c_str(), NULL, 16);
+	size_t		i = 0;
+
+	while (chunksize)
+	{
+        //FIND END OF INITAL CHUNKSIZE LABEL
+		i = coded.find("\r\n", i) + 2;
+        // SKIP PAST INITIAL SIZE \R\N LABEL
+		body += coded.substr(i, chunksize);
+		//SIZE ADDED IS CHUNKSIZE + THE \R\N AT END WHICH ISN'T INCLUDED IN CHUNKSIZE
+        i += chunksize + 2;
+		//TAKE NEXT 100
+        subchunk = coded.substr(i, coded.length() - i);
+		//GET SIZE OF NEXT CHUNK
+        chunksize = strtol(subchunk.c_str(), NULL, 16);
+	}
+	std::string decoded = head + body + "\r\n\r\n";
+    return (decoded);
+}
+
+void Server::readData(int fd, int epfd)
+{
+	int n;
+	int size = 24;
+	char buf[size];
+	std::string request;
+	int inutile;
+	n = recv(fd, buf, size - 1, 0);
+	if (n == 0)
+	{
+		std::cout << "connexion close by client" << std::endl;
+		close (fd);
+		epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL);
+	}
+	else if (n < 0) //IF WE HAVE LARGER BUFFER DO WE NEED TO ADD: && request.find_last_of("\r\n\r\n") != request.length() - 4
+	{
+		std::cout << "error with recv" << std::endl;
+		close (fd);
+		epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL);
+	}
+	else
+	{
+		request += buf;
+		request = this->processContent(request, fd, epfd);
+		this->pseudoReponse(request, fd);
+		close(fd);
+		epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL);
+	}
+}
+
+
+void Server::pseudoReponse(std::string request, int fd) //destinee a etre suprimee quand la class reponse sera faite
+{
+	std::ifstream is ("index.html", std::ifstream::binary);
+	if (!is)
+	{
+		clear_fd();
+		exit(1);
+	}
+	is.seekg (0, is.end);
+	int length = is.tellg();
+	is.seekg (0, is.beg);
+	char * b = (char *)malloc(sizeof(char) * (length + 1));
+	b[length] = '\0';
+	std::stringstream ss;
+	ss << length;
+	std::string lenstr= ss.str();
+	is.read (b,length);
+	std::string bufStr(b);
+	Request Req(request);
+	std::string goodresponse = "HTTP/1.1 200 ok\nContent-Type: text/html\nContent-Length: " + lenstr +"\n\n" + bufStr;
+	free(b);
+	char badrequest[1024] = {"HTTP/1.1 400 Bad Request\r\n\r\n<HTML><HEAD><meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\">\r\n<TITLE>Bad Request</TITLE></HEAD><BODY>\r\n<H1>Bad Request</H1>\r\n</BODY></HTML>\r\n\r\n"};
+	char badresponse[1024] = {"HTTP/1.1 404 Not Found\r\n\r\n<HTML><HEAD><meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\">\r\n<TITLE>Not Found</TITLE></HEAD><BODY>\r\n<H1>Not Found</H1>\r\n</BODY></HTML>\r\n\r\n"};
+		
+		
+		if (Req.getMethod() != "GET")	
+			send(fd, badrequest, strlen(badrequest), 0);	
+		else if (Req.getPath() == "/" || Req.getPath() == "/index.html")
+			send(fd, goodresponse.c_str(), strlen(goodresponse.c_str()), 0);
+		else
+			send(fd, badresponse, strlen(badresponse), 0);
+} //fin de la fonction  a supprimer quand Response sera faite
+
+int Server::getListen() const
+{
+	return (this->listenfd);
+}
---
+++ essais/request/Request.cpp
@@ -0,0 +1,129 @@
+#include "Request.hpp"
+
+Request::Request(const std::string& str) : _orig_req(str)
+{
+	this->parse(str);
+}
+
+Request::~Request()
+{
+}
+
+Request&	Request::operator=(const Request& copy)
+{
+    _method = copy._method;
+    _path = copy._path;
+	_version = copy._version;
+    _body = copy._body;
+    int i = 0;
+    int end = _headers.size();
+    std::map<std::string, std::string> tmp = copy.getHeaders();
+    std::map<std::string, std::string>::iterator it = tmp.begin();
+    while (i < end)
+    {
+        _headers.insert(*it);
+        i++;
+        it++;
+    }
+    
+	return *this;
+}
+
+const std::map<std::string, std::string> &Request::getHeaders(void) const
+{
+    return (_headers);
+}
+
+void Request::parse(const std::string &str)
+{
+    /*** LINE1 PARSING ***/
+    std::string line;
+    int end = str.find_first_of('\r');
+    //COLLECT FIRST LINE ONLY
+    line = str.substr(0, end);
+    int break_1 = line.find_first_of(' ');
+    //METHOD IS FROM 0 UNTIL FIRST SPACE
+    _method = line.substr(0, break_1);
+    break_1 += 1;
+    //REMOVE METHOD AND SPACE FROM LINE
+    line = line.substr(break_1, end - break_1);
+    int break_2 = line.find_first_of(' ');
+    //PATH IS FROM 0 UNTIL FIRST SPACE
+    _path = line.substr(0, break_2);
+    break_2 += 1;
+    //VERSION IS FROM AFTER PREV SPACE UNTIL END
+    _version = line.substr(break_2,  line.length() - break_2);
+    
+    
+    /*** HEADERS ***/
+    std::string tmp = str;
+    std::string key;
+    std::string val;
+    int split;
+    //DELETE FIRST LINE FROM STR
+    tmp = tmp.erase(0, (size_t)end + 2);
+    int ptr = 0;
+    int ptr_end = tmp.find_first_of('\n');
+    int count = 0;
+    //UNTIL WE GET TO AN EMPTY LINE
+    while (ptr_end != ptr + 1)
+    {
+        //COLLECT KEY AND VALUE AND INSERT IN MAP HEADERS
+        split = tmp.find_first_of(':');
+        key = tmp.substr(0, split);
+        val = tmp.substr(split + 2, ((ptr_end - 1) - (split + 2)));
+        std::pair<std::string, std::string> tmp_pair(key, val);
+        _headers.insert(tmp_pair);
+        tmp = tmp.erase(0, ptr_end + 1);
+        ptr_end = tmp.find_first_of('\n');
+        
+    }
+    //SKIP OVER EMPTY LINES
+    while (tmp.find_first_of("\r\n") == 0)
+        tmp = tmp.erase(0, 1);
+    //COLLECT BODY
+    const std::string con_key = "Content-Length";
+    _body = tmp.substr(0, tmp.length() - 4);
+    //CHECK LENGTH
+    if (_headers.find(con_key) != _headers.end())
+    {
+        if (_body.length() == (atoi(_headers[con_key].c_str())))
+            std::cout << "Content-Lenght is correct" << std::endl;
+        else
+            std::cout << "TODO: error? Len is: " << (_body.length()) << " Should be: " << (atoi(_headers[con_key].c_str())) << std::endl;
+    }
+    printer();
+}
+
+void Request::printer(void)
+{
+    std::cout << "------REQUEST INFO RECOVERED IN REQUEST CLASS------" << std::endl;
+    std::cout << "METHOD" << std::endl;
+    std::cout  << "|" << _method <<  "|" <<std::endl;
+    std::cout << "PATH" << std::endl;
+    std::cout  << "|" << _path << "|" << std::endl;
+    std::cout << "VERSION" << std::endl;
+    std::cout << "|" << _version << "|" << std::endl;
+    int i = 0;
+    int end = _headers.size();
+    std::cout << "HEADERS" << std::endl;
+    std::map<std::string, std::string>::iterator it;
+    it = _headers.begin();
+    while (i < end)
+    {
+        std::cout << "HEADER " << i << "->KEY: |" << it->first << "| VALUE: |" << it->second << "|" << std::endl;
+        i++;
+        it++;
+    }
+    std::cout << "BODY" << std::endl;
+    std::cout << "|" << _body << "|" << std::endl;
+}
+
+std::string const &Request::getMethod() const
+{
+	return (this->_method);
+}
+std::string const &Request::getPath() const
+{
+	return (this->_path);
+}
